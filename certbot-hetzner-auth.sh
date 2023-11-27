65da124a003fa43e1790caeb37e5d7a78434e10a
Felix Hummel <felix@felixhummel.de>
2023-11-27 19:16:36 +0100

    make second curl post a little more readable

--- certbot-hetzner-auth.sh
+++ certbot-hetzner-auth.sh
@@ -8,10 +8,20 @@ zone_id=$(curl -Ss \
         "https://dns.hetzner.com/api/v1/zones?search_name=${search_name}" | \
         jq ".\"zones\"[] | select(.name == \"${search_name}\") | .id" 2>/dev/null | tr -d '"')
 
+data=<<EOF
+{
+  "value": "${CERTBOT_VALIDATION}",
+  "ttl": 300,
+  "type": "TXT",
+  "name": "_acme-challenge.${CERTBOT_DOMAIN}.",
+  "zone_id": "${zone_id}"
+}
+EOF
+
 curl -Ss -X "POST" "https://dns.hetzner.com/api/v1/records" \
      -H 'Content-Type: application/json' \
      -H "Auth-API-Token: ${token}" \
-     -d "{ \"value\": \"${CERTBOT_VALIDATION}\", \"ttl\": 300, \"type\": \"TXT\", \"name\": \"_acme-challenge.${CERTBOT_DOMAIN}.\", \"zone_id\": \"${zone_id}\" }" > /dev/null 2>/dev/null
+     -d "$data"
 
 # just make sure we sleep for a while (this should be a dig poll loop)
 sleep 30
