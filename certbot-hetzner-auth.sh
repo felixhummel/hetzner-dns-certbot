d2bdc3471b0b8b88ba9cf0b0fddda2867fb231ff
Felix Hummel <felix@felixhummel.de>
2023-11-27 19:13:08 +0100

    silence curl
    From `man curl`:
    
     -S, --show-error
            When used with -s, --silent, it makes curl show an error message if it fails.
    
     -s, --silent
            Silent or quiet mode. Do not show progress meter or error messages. Makes Curl mute. It will still output the data you ask for, potentially even to the terminal/stdout unless you redirect it.
    
            Use -S, --show-error in addition to this option to disable progress meter but still show error messages.

--- certbot-hetzner-auth.sh
+++ certbot-hetzner-auth.sh
@@ -3,12 +3,12 @@
 token=$(cat /etc/hetzner-dns-token)
 search_name=$( echo $CERTBOT_DOMAIN | rev | cut -d'.' -f 1,2 | rev)
 
-zone_id=$(curl \
+zone_id=$(curl -Ss \
         -H "Auth-API-Token: ${token}" \
         "https://dns.hetzner.com/api/v1/zones?search_name=${search_name}" | \
         jq ".\"zones\"[] | select(.name == \"${search_name}\") | .id" 2>/dev/null | tr -d '"')
 
-curl -X "POST" "https://dns.hetzner.com/api/v1/records" \
+curl -Ss -X "POST" "https://dns.hetzner.com/api/v1/records" \
      -H 'Content-Type: application/json' \
      -H "Auth-API-Token: ${token}" \
      -d "{ \"value\": \"${CERTBOT_VALIDATION}\", \"ttl\": 300, \"type\": \"TXT\", \"name\": \"_acme-challenge.${CERTBOT_DOMAIN}.\", \"zone_id\": \"${zone_id}\" }" > /dev/null 2>/dev/null
