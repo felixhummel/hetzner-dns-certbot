34449bfb16550203ac05c1ee836f30212c0c095b
Felix Hummel <felix@felixhummel.de>
2023-11-27 20:14:10 +0100

    quiet curl in certbot-hetzner-cleanup.sh

--- certbot-hetzner-cleanup.sh
+++ certbot-hetzner-cleanup.sh
@@ -3,18 +3,18 @@
 token=$(cat /etc/hetzner-dns-token)
 search_name=$( echo $CERTBOT_DOMAIN | rev | cut -d'.' -f 1,2 | rev)
 
-zone_id=$(curl \
+zone_id=$(curl -Ss \
         -H "Auth-API-Token: ${token}" \
         "https://dns.hetzner.com/api/v1/zones?search_name=${search_name}" | \
         jq ".\"zones\"[] | select(.name == \"${search_name}\") | .id" 2>/dev/null | tr -d '"')
 
-record_ids=$(curl \
+record_ids=$(curl -Ss \
         -H "Auth-API-Token: $token" \
         "https://dns.hetzner.com/api/v1/records?zone_id=$zone_id" | \
        jq ".\"records\"[] | select(.name == \"_acme-challenge.${CERTBOT_DOMAIN}.\") | .id" 2>/dev/null | tr -d '"')
 
 for record_id in $record_ids
 do
-        curl -H "Auth-API-Token: $token" \
+        curl -Ss -H "Auth-API-Token: $token" \
                 -X "DELETE" "https://dns.hetzner.com/api/v1/records/${record_id}" > /dev/null 2> /dev/null
 done
